a5320b57143240da9e9a22bb2ffe3e7436229f5d
ufukanilozluk <ufukanilozluk@gmail.com>
2024-07-23 16:17:14 +0300

    REFACTOR:
    Some UI changes

--- WeatherbyOzluk/UI/Reusables/CustomNavigationController.swift
+++ WeatherbyOzluk/UI/Reusables/CustomNavigationController.swift
@@ -3,22 +3,32 @@ import UIKit
 
 final class CustomNavigationController: UINavigationController {
   override init(rootViewController: UIViewController) {
-  super.init(rootViewController: rootViewController)
-  applyTheme()
+    super.init(rootViewController: rootViewController)
+    applyTheme()
   }
   required init?(coder aDecoder: NSCoder) {
-  super.init(coder: aDecoder)
-  applyTheme()
+    super.init(coder: aDecoder)
+    applyTheme()
   }
   override func viewDidLoad() {
     super.viewDidLoad()
     applyTheme()
   }
   private func applyTheme() {
-  // Navigation Bar title text attributes
-  let textAttributes = [NSAttributedString.Key.foregroundColor: Colors.tint]
-  navigationBar.titleTextAttributes = textAttributes
-  navigationBar.barTintColor = Colors.tint
-  navigationBar.tintColor = Colors.tint
+    let appearance = UINavigationBarAppearance()
+    appearance.backgroundColor = UIColor.systemBackground
+    appearance.titleTextAttributes = [.foregroundColor: UIColor.label]
+    appearance.largeTitleTextAttributes = [.foregroundColor: UIColor.label]
+    appearance.shadowColor = nil
+    if #available(iOS 15.0, *) {
+      navigationBar.scrollEdgeAppearance = appearance
+      navigationBar.standardAppearance = appearance
+      navigationBar.compactAppearance = appearance
+      navigationBar.compactScrollEdgeAppearance = appearance
+    } else {
+      navigationBar.standardAppearance = appearance
+      navigationBar.scrollEdgeAppearance = appearance
+    }
+    navigationBar.tintColor = Colors.tint  // Buton ve diğer öğelerin rengi
   }
 }
--- WeatherbyOzluk/UI/Scenes/NewCity/AddCityViewController.swift
+++ WeatherbyOzluk/UI/Scenes/NewCity/AddCityViewController.swift
@@ -33,6 +33,7 @@ final class AddCityViewController: UIViewController {
     configureSearchController()
     setSearchTextField()
     setBindings()
+    registerForKeyboardNotifications()
   }
 
   private func configureTableView() {
@@ -63,6 +64,33 @@ final class AddCityViewController: UIViewController {
       self?.cityNames = cityNames
     }
   }
+  
+  deinit {
+    unregisterForKeyboardNotifications()
+  }
+  private func registerForKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    private func unregisterForKeyboardNotifications() {
+      NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+      NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+
+    @objc private func keyboardWillShow(_ notification: Notification) {
+      if let userInfo = notification.userInfo,
+        let keyboardFrame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as? CGRect {
+          let keyboardHeight = keyboardFrame.height
+          let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: keyboardHeight, right: 0)
+          citiesTableView.contentInset = contentInsets
+          citiesTableView.scrollIndicatorInsets = contentInsets
+      }
+    }
+    @objc private func keyboardWillHide(_ notification: Notification) {
+      let contentInsets = UIEdgeInsets.zero
+      citiesTableView.contentInset = contentInsets
+      citiesTableView.scrollIndicatorInsets = contentInsets
+    }
 
   // MARK: - Search TextField Customization
   private func setSearchTextField() {
